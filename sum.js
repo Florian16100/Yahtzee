e3e815105ec0eccbc35f4742e4ce09f6e8f398c2
Florian16100 <florian.lys@ynov.com>
2021-10-09 19:26:41 +0200

    Brelan et Carré

--- sum.js
+++ sum.js
@@ -3,27 +3,27 @@ module.exports ={
         return this.filter(predicate).length;
     },
     
-    One: function firstLancerOne(rolls){
+    One: function LancerUn(rolls){
         return this.Count(rolls, 1)
     },
     
-    Two: function firstLancerTwo(rolls) {
+    Two: function LancerDeux(rolls) {
         return this.Count(rolls, 2)
     },
 
-    Three: function firstLancerThree(rolls) {
+    Three: function LancerTrois(rolls) {
         return this.Count(rolls, 3)
     },
 
-    Four: function firstLancerFour(rolls) {
+    Four: function LancerQuatre(rolls) {
         return this.Count(rolls, 4)
     },
 
-    Five: function firstLancerFive(rolls) {
+    Five: function LancerCinq(rolls) {
         return this.Count(rolls, 5)
     },
 
-    Six: function firstLancerSix(rolls) {
+    Six: function LancerSix(rolls) {
         return this.Count(rolls, 6)
     },
 
@@ -31,4 +31,17 @@ module.exports ={
         let test = rolls.count(dice => dice == value);
         return value * test;
     },
+
+    Brelan: function LancerBrelan(rolls) {
+        return (data) => FinalSomme(rolls)(data) ? somme(data) : 0
+    },
+
+    Square: function LancerSquare(rolls) {
+        return (data) => FinalSomme(rolls)(data) ? somme(data) : 0
+    },
+
+    FinalSomme: function CalculName(rolls) {
+        return (data) => FinalCalcul(data).somme(x => x.count >= rolls);
+    },
+   
 }
--- sum.test.js
+++ sum.test.js
@@ -4,6 +4,9 @@ const LancerTrois = require("./sum");
 const LancerQuatre = require("./sum");
 const LancerCinq = require("./sum");
 const LancerSix = require("./sum");
+const LancerBrelan = require("./sum");
+const LancerSquare = require("./sum");
+
 
 const dataOne = [
     {
@@ -20,8 +23,8 @@ const dataOne = [
     }
   ];
 
-  describe.each(dataOne)(`nombre de un `, (one) => {
-    it(`le résultat ${one.rolls} correspond à ${one.result}`, () => {
+  describe.each(dataOne)(`Nombre de 1 : `, (one) => {
+    it(`Le résultat de ${one.rolls} est ${one.result}`, () => {
       const result = LancerUn.One(one.rolls);
       expect(result).toBe(one.result);
     });
@@ -43,8 +46,8 @@ const dataOne = [
     }
   ];
 
-  describe.each(dataTwo)(`nombre de deux `, (two) => {
-    it(`le résultat ${two.rolls} correspond à ${two.result}`, () => {
+  describe.each(dataTwo)(`Nombre de 2 : `, (two) => {
+    it(`Le résultat ${two.rolls} est ${two.result}`, () => {
       const result = LancerDeux.Two(two.rolls);
       expect(result).toBe(two.result);
     });
@@ -65,8 +68,8 @@ const dataOne = [
     }
   ];
 
-  describe.each(dataThree)(`nombre de trois `, (three) => {
-    it(`le résultat ${three.rolls} correspond à ${three.result}`, () => {
+  describe.each(dataThree)(`Nombre de 3 : `, (three) => {
+    it(`Le résultat ${three.rolls} est ${three.result}`, () => {
       const result = LancerTrois.Three(three.rolls);
       expect(result).toBe(three.result);
     });
@@ -88,8 +91,8 @@ const dataOne = [
     }
   ];
 
-  describe.each(dataFour)(`nombre de quatre `, (four) => {
-    it(`le résultat ${four.rolls} correspond à ${four.result}`, () => {
+  describe.each(dataFour)(`Nombre de 4 : `, (four) => {
+    it(`Le résultat ${four.rolls} est ${four.result}`, () => {
       const result = LancerQuatre.Four(four.rolls);
       expect(result).toBe(four.result);
     });
@@ -111,8 +114,8 @@ const dataOne = [
     }
   ];
 
-  describe.each(dataFive)(`nombre de cinq `, (five) => {
-    it(`le résultat ${five.rolls} correspond à ${five.result}`, () => {
+  describe.each(dataFive)(`Nombre de 5 : `, (five) => {
+    it(`Le résultat ${five.rolls} est ${five.result}`, () => {
       const result = LancerCinq.Five(five.rolls);
       expect(result).toBe(five.result);
     });
@@ -133,9 +136,61 @@ const dataOne = [
     }
   ];
 
-  describe.each(dataSix)(`nombre de six `, (six) => {
-    it(`le résultat ${six.rolls} correspond à ${six.result}`, () => {
+  describe.each(dataSix)(`Nombre de 6 : `, (six) => {
+    it(`Le résultat ${six.rolls} est ${six.result}`, () => {
       const result = LancerSix.Six(six.rolls);
       expect(result).toBe(six.result);
     });
   });
+
+  const dataBrelan = [
+    {
+      rolls: [1,2,3,4,5], 
+      result: 0
+    },
+    {
+      rolls: [1,2,1,1,5], 
+      result: 10
+    },
+    {
+      rolls: [4,2,4,3,4], 
+      result: 17
+    },
+    {
+      rolls: [6,5,6,2,6], 
+      result: 25
+    }
+ ]
+ describe.each(dataBrelan)('Résultat pour un Brelan :', ({rolls, result}) => {
+     it(`Pour ${rolls} le score est ${result}`, () => {
+         const result = LancerBrelan.Brelan('brellan', rolls)
+         expect(result).toBe(result);
+     });
+ });
+ 
+ 
+ 
+ const dataSquare = [
+     {
+       rolls: [1,2,3,4,5], 
+       result: 0
+      },
+     {
+       rolls: [1,2,2,2,2], 
+       result: 9
+      },
+     {
+       rolls: [1,1,1,1,1], 
+       result: 5
+      },
+     {
+       rolls: [3,3,6,3,6], 
+       result: 0
+      }
+ ]
+ describe.each(dataSquare)('Résultat pour un carré :', ({rolls, result}) => {
+     it(`Pour ${rolls} le resultat est ${result}`, () => {
+         const result = LancerSquare.Square('carré', rolls)
+         expect(result).toBe(result);
+     });
+ });
